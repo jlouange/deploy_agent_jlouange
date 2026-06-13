802308bfbfb80a68090abe7e326f70f3cae33231
jlouange <j.mushumba@alustudent.com>
2026-06-13 11:43:44 +0200

    Added environment validation

--- setup_project.sh
+++ setup_project.sh
@@ -2,3 +2,18 @@
 echo "============================="
 echo "Attendance tracker deployment"
 echo "============================="
+
+#checking wether python is on the system
+if python3 --version &> /dev/null; then
+    echo "Python is installed on the system"
+    echo "---------------------------------"
+
+else
+    echo "----------------------------------"
+    echo -e "Python is not found! \n First install python"
+    exit 1
+fi
+
+echo "========================================"
+
+#
