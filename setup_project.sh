3939837aef014cfa8875c94c7a380a38dbf546cb
jlouange <j.mushumba@alustudent.com>
2026-06-13 16:16:44 +0200

    Added dynamic configuration

--- setup_project.sh
+++ setup_project.sh
@@ -40,16 +40,21 @@ cp attendance_checker.py "$directory_name/"
 
 if [ -f "$directory_name/attendance_checker.py" ]; then
     echo "Deployment verified: attendance_checker.py is present."
+    echo "------------------------------------------------------"
 else
     echo "Error: attendance_checker.py missing."
+    echo "------------------------------------------------------"
 fi
 
 cp assets.csv "$directory_name"/Helpers
 
 if [ -f "$directory_name/Helpers/assets.csv" ]; then
     echo "Deployment verified: assets.csv is present."
+    echo "-------------------------------------------------------"
+
 else
     echo "Error: assets.csv missing."
+    echo "----------------------------------------------------------"
 fi
 
 cp config.json "$directory_name"/Helpers
@@ -58,15 +63,50 @@ if [ -f "$directory_name/Helpers/config.json" ]; then
     echo "Deployment verified: config.json is present."
 else
     echo "Error: config.json missing."
+    echo "---------------------------------------------"
 fi
 
 cp reports.log "$directory_name"/reports
 
 if [ -f "$directory_name/reports/reports.log" ]; then
     echo "Deployment verified: reports.log is present."
+    echo "--------------------------------------------"
 else
     echo "Error: reports.log missing."
+    echo "---------------------------------------------"
 fi
 
+echo "=================================="
 
+#updating threshold values
+read -r -p "You want to update the attendance thresholds?[Y/N]: " choice
 
+case "$choice" in
+  [Yy]*)
+    read -r -p "Enter warning threshold(default 75%): " warning
+    warning=${warning:-75}
+    warning=${warning%[%]*}
+    read -r -p "Enter failure threshold(default 50%): " failure
+    failure=${failure:-50}
+    failure=${failure%[%]*}
+
+    if [[ "$warning" =~ ^[0-9]+$ && "$failure" =~ ^[0-9]+$ ]]; then
+
+      sed -i "s/75/$warning/g" "$directory_name/Helpers/config.json"
+      sed -i "s/50/$failure/g" "$directory_name/Helpers/config.json"
+
+      echo "Threshold updated successfully to $warning% and $failure%"
+    else
+      echo "Error: Thresholds must be numeric values. Update aborted."
+
+    fi
+    
+
+    ;;
+  [Nn]*)
+      echo "Keeping default thresholds (75% and 50%)"
+    ;;
+    *)
+      echo "Invalid input skipping updates..."
+      exit 1
+esac
